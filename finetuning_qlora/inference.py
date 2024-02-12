875387362a9ecfdd73169bdbb6d5d013a6805cba
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-02-12 12:03:53 +0700

    #code

--- finetuning_qlora/inference.py
+++ finetuning_qlora/inference.py
@@ -1,17 +1,20 @@
 import peft
 from helpers.utils import load_params
 from peft import PeftModel, PeftConfig
-import logging
-"""
-load model: including quantization config, load_in4bit
-get peft model
-model.eval()
-"""
 
 def main():
+    """
+    Inference with fine tuned model
+    PEFT model turns out can be considered as Adapter because it takes
+    trainable params and trains these params, for inference we take these
+    params by PeftModel class that take fine-tuned params
+    """
     params = load_params("../config/config.yml") # params file
     """
     load pre-trained peft saved model for inference
     """
     model = PeftModel.from_pretrained(params['saved_model']) # peft saved model
     print(f"Model info: {model.state_dict()}") # logging model
+
+if __name__ == "__main__":
+    main()
